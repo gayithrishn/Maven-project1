6e2735f9cef8afb284451417dc256e4b6af12739
gayithrishn <gayithrishn@gmail.com>
2025-03-24 17:29:25 +0530

    updated code

--- src/main/java/com/mvit/App.java
+++ src/main/java/com/mvit/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int a=10 , b=20 ;
+        int sum=a+b;
+        System.out.println("sum is :", sum);
     }
 }
